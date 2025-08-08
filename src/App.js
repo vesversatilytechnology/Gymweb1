a11e9889167064f9851727b01fc8da53f3ac41f1
samuel <samueldias3004@hotmail.com>
2025-08-08 01:30:09 -0300

    Ajuste no Dashboard.jsx

--- src/App.js
+++ src/App.js
@@ -3,15 +3,35 @@ import Auth from "./pages/Auth";
 import Dashboard from "./pages/Dashboard";
 import AdminCatalog from "./pages/AdminCatalog";
 import AdminTreinos from "./pages/AdminTreinos";
+import AdminRoute from "./components/AdminRoute"; // protege rotas de admin
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Login / Registro */}
         <Route path="/" element={<Auth />} />
+
+        {/* Dashboard normal */}
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/admin/catalog" element={<AdminCatalog />} />
-        <Route path="/admin/treinos" element={<AdminTreinos />} />
+
+        {/* Rotas protegidas para treinador */}
+        <Route
+          path="/admin/catalog"
+          element={
+            <AdminRoute>
+              <AdminCatalog />
+            </AdminRoute>
+          }
+        />
+        <Route
+          path="/admin/treinos"
+          element={
+            <AdminRoute>
+              <AdminTreinos />
+            </AdminRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
